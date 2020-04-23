704227f4f7e237fafb3262e2b6ac4cdb2ce6e45d
Dan-Mikkel Hübenette <danmikkelhubenette@gmail.com>
2020-04-23 11:32:02 +0200

    Initial commit

---
+++ src/main/java/com/nanodami/springrecipedemoproject/SpringrecipedemoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.nanodami.springrecipedemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrecipedemoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrecipedemoprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nanodami/springrecipedemoproject/SpringrecipedemoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanodami.springrecipedemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrecipedemoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
