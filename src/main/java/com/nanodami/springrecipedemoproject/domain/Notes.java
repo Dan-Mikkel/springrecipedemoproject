3ef9f39835177317b83c2b492c6161acedaeb225
Dan-Mikkel Hübenette <danmikkelhubenette@gmail.com>
2020-08-19 18:13:37 +0200

    Added getters and setters.

--- src/main/java/com/nanodami/springrecipedemoproject/domain/Notes.java
+++ src/main/java/com/nanodami/springrecipedemoproject/domain/Notes.java
@@ -1,5 +1,30 @@
 package com.nanodami.springrecipedemoproject.domain;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Notes {
     private String notes;
+
+    //Relationships
+
+    private Recipe recipes;
+
+    //Getters and setters
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Recipe getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Recipe recipes) {
+        this.recipes = recipes;
+    }
 }
--- src/main/java/com/nanodami/springrecipedemoproject/domain/Recipe.java
+++ src/main/java/com/nanodami/springrecipedemoproject/domain/Recipe.java
@@ -1,5 +1,8 @@
 package com.nanodami.springrecipedemoproject.domain;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Recipe {
 
     private String description;
@@ -12,5 +15,87 @@ public class Recipe {
     private Difficulty difficulty;
 
     private byte[] image;
+    private Notes notes;
 
+    // Getters and setters
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(Integer prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public Integer getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(Integer cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public Integer getServings() {
+        return servings;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public Notes getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Notes notes) {
+        this.notes = notes;
+    }
 }
