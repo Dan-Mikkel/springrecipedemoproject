67ceb564c01730bfe79f69590610135ce0d6ec89
Dan-Mikkel Hübenette <danmikkelhubenette@gmail.com>
2020-08-20 10:06:46 +0200

    Satte opp et forhold mellom UnitOfMeasure og Ingredient

--- src/main/java/com/nanodami/springrecipedemoproject/domain/Ingredient.java
+++ src/main/java/com/nanodami/springrecipedemoproject/domain/Ingredient.java
@@ -11,8 +11,13 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
+    //Relationships
+
     @ManyToOne
     private Recipe recipe;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure unitOfMeasure;
 
     public Long getId() {
         return id;
@@ -44,5 +49,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
     }
 }
